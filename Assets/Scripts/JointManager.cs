23a0c64c038fe9e1ece3e805809e5e5320bbc3ac
Hypersebula <hypersebula@gmail.com>
2025-09-09 21:03:00 +0200

    ManagerScriptUpdate
    building onto the manager script, progress but more trouble

--- Assets/Scripts/JointManager.cs
+++ Assets/Scripts/JointManager.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 [System.Serializable]
 public class Settings
 {
     public string Name;
 
-    public float Strenght = 0;
-    public float spring = 0;
-    public float damper = 0;
+    public float localStrenghtMultiplier;
 
-    public float twistLimit = 0;
-    public float swingLimit = 0;
+    public float localSpring = 0f;
+    public float localDamper = 0f;
+
+    public float twistLimit = 0f;
+    public float swingLimit = 0f;
 }
 
 public class JointManager : MonoBehaviour
@@ -22,7 +24,22 @@ public class JointManager : MonoBehaviour
     public bool AngularMotionLimited = false;
     public bool twistLimited = false;
     public bool swingLimited = false;
-    public bool ApplyStrenght = false;
+    public bool UpdateStrenght = false;
+
+    [Header("Strenght")]
+    public float globalStrenghtMultiplier = 1f;
+
+    public float globalSpring = 100f;
+    public float globalDamper = 10f;
+    public float maxForce = 1000f;
+
+    private List<ConfigurableJoint> strenghtJoints = new List<ConfigurableJoint>();
+
+    private void Awake()
+    {
+        strenghtJoints.AddRange(GetComponentsInChildren<ConfigurableJoint>());
+        ApplyStrenght();
+    }
 
     public void RunSettings()
     {
@@ -30,6 +47,16 @@ public class JointManager : MonoBehaviour
 
         foreach (var joint in joints)
         {
+            // Try to find a Settings with the same name as joint gameObject
+            foreach (var b in bones)
+            {
+                if (b.Name == joint.gameObject.name)
+                {
+                    settings = b;
+                    break;
+                }
+            }
+
             // Apply angular Limits
             if (AngularMotionLimited)
             {
@@ -53,20 +80,22 @@ public class JointManager : MonoBehaviour
                 joint.angularYLimit = swingY;
                 joint.angularZLimit = swingZ;
             }
+        }
+    }
 
-            // Apply Drive
-            JointDrive drive = new JointDrive
-            {
-                positionSpring = settings.spring,
-                positionDamper = settings.damper,
-            };
-            joint.rotationDriveMode = RotationDriveMode.XYAndZ;
+    public void ApplyStrenght()
+    {
+        foreach (var joint in strenghtJoints)
+        {
+            JointDrive drive = new JointDrive();
+            drive.positionSpring = globalSpring * globalStrenghtMultiplier;
+            drive.positionDamper = globalDamper * globalStrenghtMultiplier;
+            drive.maximumForce = maxForce;
+
+            joint.slerpDrive = drive;
+
             joint.angularXDrive = drive;
             joint.angularYZDrive = drive;
-
-            // Apply Strenght
-            
-
         }
     }
 }
