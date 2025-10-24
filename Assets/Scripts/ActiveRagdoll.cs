5c05e4894561239b3650687618eba8e56e6ee202
Hypersebula <hypersebula@gmail.com>
2025-10-24 23:39:11 +0200

    tutorialFollow&ActiveRagdollScript
    Right now following a good tutorial, added an Active ragdoll script that matches the bones to the joints of both of the characters.

--- Assets/Scripts/ActiveRagdoll.cs
+++ Assets/Scripts/ActiveRagdoll.cs
@@ -1,72 +1,27 @@
 using UnityEngine;
-using System.Collections.Generic;
+using System.Collections;
 
-public class RagdollFollower : MonoBehaviour
+public class ActiveRagdoll : MonoBehaviour
 {
-    public Transform animatedRoot;
-    public Transform ragdollRoot;
-
-    // Each animated bone -> joint and its bind-pose offset
-    private class BoneData
-    {
-        public ConfigurableJoint joint;
-        public Quaternion rotationOffset;
-    }
-
-    private Dictionary<Transform, BoneData> boneMap = new Dictionary<Transform, BoneData>();
+    public Transform[] animated;
+    public ConfigurableJoint[] joints;
+    private Quaternion[] startPos;
 
     void Start()
     {
-        // Build map and cache offsets in the bind pose
-        var ragdollJoints = ragdollRoot.GetComponentsInChildren<ConfigurableJoint>();
-        foreach (var joint in ragdollJoints)
+        startPos = new Quaternion[joints.Length];
+
+        for (int i = 0; i < joints.Length; i++)
         {
-            Transform animatedBone = FindChildByName(animatedRoot, joint.transform.name);
-            if (animatedBone == null) continue;
-
-            BoneData data = new BoneData();
-            data.joint = joint;
-
-            // Capture offset between animated bone and ragdoll joint at rest pose
-            // joint.connectedBody is the parent bone
-            if (joint.connectedBody != null)
-            {
-                Quaternion animatedLocal = Quaternion.Inverse(joint.connectedBody.transform.rotation) * animatedBone.rotation;
-                Quaternion ragdollLocal = Quaternion.Inverse(joint.connectedBody.transform.rotation) * joint.transform.rotation;
-                data.rotationOffset = Quaternion.Inverse(animatedLocal) * ragdollLocal;
-            }
-            else
-            {
-                data.rotationOffset = Quaternion.identity;
-            }
-
-            boneMap[animatedBone] = data;
+            startPos[i] = joints[i].transform.localRotation;
         }
     }
 
-    void LateUpdate()
+    void Update()
     {
-        foreach (var kvp in boneMap)
+        for (int i = 0; i < joints.Length; i++)
         {
-            Transform animatedBone = kvp.Key;
-            BoneData data = kvp.Value;
-            ConfigurableJoint joint = data.joint;
-
-            if (joint.connectedBody == null) continue;
-
-            // Calculate desired local rotation relative to parent (connected body)
-            Quaternion targetLocalRotation =
-                Quaternion.Inverse(joint.connectedBody.transform.rotation) * animatedBone.rotation;
-
-            // Apply the cached offset so ragdoll matches animated orientation
-            joint.targetRotation = targetLocalRotation * data.rotationOffset;
+            joints[i].targetRotation = animated[i].transform.localRotation;
         }
-    }
-
-    Transform FindChildByName(Transform parent, string name)
-    {
-        foreach (Transform t in parent.GetComponentsInChildren<Transform>())
-            if (t.name == name) return t;
-        return null;
     }
 }
