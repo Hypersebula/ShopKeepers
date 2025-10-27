0eafc93cfaf37efac1bd10c0d5aaef670fd63434
Hypersebula <hypersebula@gmail.com>
2025-10-27 09:28:38 +0100

    LegsStepping
    Made the animated legs step by a FootPlacement script.

--- Assets/Scripts/FootPlacement.cs
+++ Assets/Scripts/FootPlacement.cs
@@ -2,5 +2,71 @@ using UnityEngine;
 
 public class FootPlacement : MonoBehaviour
 {
-    
+    Vector3 currentPosition;
+    Vector3 newPosition;
+    Vector3 oldPosition;
+
+    [SerializeField] Transform hips;
+
+    [SerializeField] float legSpacing;
+    [SerializeField] float stepDistance;
+    [SerializeField] float stepHeight;
+    [SerializeField] float speed;
+    [SerializeField] float hipOffset;
+
+    float lerp;
+
+    public bool isStepping;
+
+    LayerMask layerMask;
+
+    [SerializeField] FootPlacement otherFeet;
+
+    private void Awake()
+    {
+        layerMask = LayerMask.GetMask("Ground");
+    }
+
+    private void Start()
+    {
+        Ray ray = new Ray(hips.position + (Vector3.right * legSpacing), Vector3.down);
+        if (Physics.Raycast(ray, out RaycastHit info, 2, layerMask))
+            newPosition = info.point; transform.position = newPosition;
+    }
+
+    private void Update()
+    {
+        transform.position = currentPosition;
+
+        Ray ray = new Ray(hips.position + (hips.right * legSpacing) + (hips.forward * hipOffset), Vector3.down);
+        if(Physics.Raycast(ray, out RaycastHit info, 2, layerMask))
+        {
+            if(Vector3.Distance(newPosition, info.point) > stepDistance && !otherFeet.isStepping)
+            {
+                lerp = 0;
+                newPosition = info.point;
+            }
+        }
+        if(lerp < 1)
+        {
+            isStepping = true;
+
+            Vector3 footPosition = Vector3.Lerp(oldPosition, newPosition, lerp);
+            footPosition.y += Mathf.Sin(lerp * Mathf.PI) * stepHeight;
+
+            currentPosition = footPosition;
+            lerp += Time.deltaTime * speed;
+        }
+        else
+        {
+            isStepping = false;
+            oldPosition = newPosition;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(newPosition, 0.1f);
+    }
 }
