05fdd7aaf8a3ca0c07826bdd5c9c966d495c24a4
Hypersebula <hypersebula@gmail.com>
2025-10-28 19:36:15 +0100

    SpringFollower
    Added a script mainly created by ChatGPT that is making the animated character follow the ragdoll hips smoothly, the smooth values is manipulated by the hips speed, if the ragdoll isnt moving, smoothness = 0 (not following). Next up : Hands positioning, WASD movement, first person look around.

--- Assets/Scripts/SpeedTracker.cs
+++ Assets/Scripts/SpeedTracker.cs
@@ -6,6 +6,7 @@ public class SpeedTracker : MonoBehaviour
     Vector3 currentVelocity;
 
     public float Speed;
+    public float horizontalSpeed;
 
     private void Update()
     {
@@ -14,7 +15,12 @@ public class SpeedTracker : MonoBehaviour
         previousPosition = transform.position;
 
         Vector3 velocity = new Vector3(currentVelocity.x, currentVelocity.y, currentVelocity.z);
+        Vector3 horizontalVelocity = new Vector2(currentVelocity.x, currentVelocity.z);
+
         Speed = velocity.magnitude;
+        horizontalSpeed = horizontalVelocity.magnitude;
+
         Speed = Mathf.Round(Speed * 10f) / 10f;
+        horizontalSpeed = Mathf.Round(horizontalSpeed * 10f) / 10f;
     }
 }
