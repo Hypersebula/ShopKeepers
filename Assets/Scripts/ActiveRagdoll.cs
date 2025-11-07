9185e15de8c580e1c3cac44f4087b534502d15f3
Hypersebula <hypersebula@gmail.com>
2025-11-07 21:07:42 +0100

    BetterCameraLook
    The looking around is smoother and better, the problem with movement overshoot is still there, tried matching velocity to the animated character (led by ChatGPT) but failed.

--- Assets/Scripts/ActiveRagdoll.cs
+++ Assets/Scripts/ActiveRagdoll.cs
@@ -3,25 +3,45 @@ using System.Collections;
 
 public class ActiveRagdoll : MonoBehaviour
 {
+    public float angularVelocityGain = 50f;
+
     public Transform[] animated;
     public ConfigurableJoint[] joints;
     private Quaternion[] startPos;
+    private Quaternion[] previousRotations;
 
     void Start()
     {
         startPos = new Quaternion[joints.Length];
+        previousRotations = new Quaternion[joints.Length];
 
         for (int i = 0; i < joints.Length; i++)
         {
             startPos[i] = joints[i].transform.localRotation;
+            previousRotations[i] = animated[i].localRotation;
         }
     }
 
-    void Update()
+    void FixedUpdate()
     {
         for (int i = 0; i < joints.Length; i++)
         {
+            ConfigurableJoint joint = joints[i];
+            Transform target = animated[i];
+            Rigidbody body = joint.GetComponent<Rigidbody>();
+
             joints[i].SetTargetRotationLocal(animated[i].localRotation, startPos[i]);
+
+            // Compute angular velocity of animated bone manually
+            Quaternion delta = target.localRotation * Quaternion.Inverse(previousRotations[i]);
+            delta.ToAngleAxis(out float angle, out Vector3 axis);
+            if (angle > 180f) angle -= 360f;
+
+            Vector3 targetAngularVelocity = axis * angle * Mathf.Deg2Rad / Time.fixedDeltaTime;
+            Vector3 velError = targetAngularVelocity - body.angularVelocity;
+            body.AddTorque(velError * angularVelocityGain, ForceMode.Acceleration);
+
+            previousRotations[i] = target.localRotation;
         }
     }
 }
