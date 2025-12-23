c9c1bf54ca63294c8b422f08d6ba45b901d49dbb
Hypersebula <hypersebula@gmail.com>
2025-12-23 21:12:13 +0100

    LegContraintMisbihaving
    The Two bone IK-constraints dont work properly on the legs, they just aim in a totaly different direction. The problem isnt in the ragdoll, its in the  animated character.

--- Assets/Scripts/LegTarget.cs
+++ Assets/Scripts/LegTarget.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class LegTarget : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Tooltip("The base for target positions")]
+    public Transform hips;
+
+    public LayerMask Ground;
+
+    [Header("Values")]
+    public float legSpacing = 0.25f;
+
+    Vector3 newPosition;
+    Vector3 currentPosition;
+    Vector3 oldPosition;
+
+    private void LateUpdate()
     {
-        
+        transform.position = currentPosition; // Match the position to the moving target
+
+        Ray ray = new Ray(hips.position + (hips.right * legSpacing), -hips.up);
+        if (Physics.Raycast(ray, out RaycastHit info, 2, Ground))
+        {
+            newPosition = info.point;
+        }
+
+        currentPosition = newPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(newPosition, 0.1f);
     }
 }
