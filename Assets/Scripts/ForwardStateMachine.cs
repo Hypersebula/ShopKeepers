32271f5681828bf55ff7f09f9191d40598457f92
Hypersebula <hypersebula@gmail.com>
2026-01-30 20:19:32 +0100

    CameraAdjustingFailed&CameraAndLookingIKFix
    I tried to add the camera adjusting by using the forward/backward camera rig, but failder multiple times. The adjusting either doesnt look natural or isnt strong enought, I fixed it for the moment by dicreasing the constraint weight of the spine Look and moving the camera to a slightly better position.

--- Assets/Scripts/ForwardStateMachine.cs
+++ Assets/Scripts/ForwardStateMachine.cs
@@ -2,5 +2,23 @@ using UnityEngine;
 
 public class ForwardStateMachine : MonoBehaviour
 {
-    
+    public Rigidbody rb;
+
+    public bool movingForward = false;
+    public bool movingBackward = false;
+    public bool idle = false;
+
+    public float treshold = 0.2f;
+
+    private void FixedUpdate()
+    {
+        Vector3 velocity = rb.linearVelocity;
+        Vector3 moveDir = velocity.normalized;
+
+        float forwardDot = Vector3.Dot(transform.forward, moveDir);
+
+        movingForward = forwardDot > treshold;
+        movingBackward = forwardDot < -treshold;
+        idle = forwardDot < treshold && forwardDot > -treshold;
+    }
 }
